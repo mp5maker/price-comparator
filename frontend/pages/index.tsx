b7980f8197bb3934d518bab0025fc8c50b2acfde
Shabuktagin Photon Khan <khan.photon@gmail.com>
2021-02-12 21:37:43 +0600

    feat: changed the respective frontend related to backend relation changes

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -32,24 +32,34 @@ const IndexPage: React.FC<IndexPropsInterface> = ({ products }) => {
 
   return (
     <div className={styles.indexContainer}>
-      <MetaData title={'Compare Prices'} />
+      <MetaData title={"Compare Prices"} />
       <Table
         list={products}
         display={{
           thead: ({ property }) => {
-            return property.toUpperCase()
+            return property.toUpperCase();
           },
           tbody: ({ property, row }) => {
-            if (property == "distributor")
-              return get(row, "distributor.name", "");
-            if (property == "type") return get(row, "type.name", "");
+            if (property == "distributors" || property == "types") {
+              return (
+                <div>
+                  {get(row, property, []).map((item: any, index: number) => {
+                    return <div key={index}>{get(item, "name", "")}</div>;
+                  })}
+                </div>
+              );
+            }
             if (property == "photos")
               return (
                 <div>
                   {get(row, property, []).map((item: any, index: number) => {
                     return (
                       <img
-                        src={`http://localhost:4000/${get(item, "filename", "")}`}
+                        src={`http://localhost:4000/images/${get(
+                          item,
+                          "filename",
+                          ""
+                        )}`}
                         width={100}
                         height={100}
                         key={index}
@@ -61,7 +71,7 @@ const IndexPage: React.FC<IndexPropsInterface> = ({ products }) => {
             return get(row, property, "");
           },
         }}
-        properties={["name", "model", "distributor", "type", "price", "photos"]}
+        properties={["name", "model", "distributors", "types", "price", "photos"]}
       />
     </div>
   );
