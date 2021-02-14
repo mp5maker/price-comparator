492849db1d3e7571b0acde868a00de0dc8ecc1b2
Shabuktagin Photon Khan <khan.photon@gmail.com>
2021-02-14 22:38:28 +0600

    fix: will complete report tomorrow

--- backend/src/routes/report.ts
+++ backend/src/routes/report.ts
@@ -1,7 +1,6 @@
 import express from "express";
 import { RESPONSE_STATUS } from "../constants/settings";
 import database from "../database";
-import { BASIC_PRODUCT_QUERY_FIELDS } from "../queries/basic-product-query";
 import responseHelper from "../utilities/responseHelper";
 
 const router = express.Router();
@@ -14,22 +13,23 @@ database.init();
  */
 router.get("/distributor-types", async (_request, response) => {
   try {
-    const product = await database.distributorRespository.createQueryBuilder("distributor")
-    .select([
-      "distributor.id",
-      "distributor.name",
-      "distributor.alias",
-      // "products.id",
-      // "products.alias",
-      // "products.name",
-      // "types.id",
-      // "types.alias",
-      // "types.name"
-    ])
-    .groupBy("distributor.id")
-    .addGroupBy("products.id")
-    .getMany()
-    response.status(RESPONSE_STATUS.OK).json(product);
+    const products = await database.productRepository
+      .createQueryBuilder("product")
+      .leftJoinAndSelect("product.distributors", "distributors")
+      .leftJoinAndSelect("product.types", "types")
+      .select([
+        "product.id",
+        "product.name",
+        "product.alias",
+        "distributors.id",
+        "distributors.alias",
+        "distributors.name",
+        "types.id",
+        "types.alias",
+        "types.name",
+      ])
+      .getMany();
+    response.status(RESPONSE_STATUS.OK).json(products);
   } catch (error) {
     return responseHelper.error.unknown({ error, response });
   }
